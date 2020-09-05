761f07ddebe7944e4c072eea5638df8f1d48be12
Trip Richert <trip.richert@gmail.com>
2020-09-05 12:38:17 -0500

    added MIT license

--- tcl/gen_xactip.tcl
+++ tcl/gen_xactip.tcl
@@ -1,3 +1,5 @@
+#see copywrite notice(s) at bottom of file
+
 puts "executing $argv0"
 puts "argv is:$argv"
 
@@ -131,3 +133,11 @@ if {![diction::checkForKey "-nodelete" {*}$argv]} {
 }
 
 puts "Completed Execution of $argv0"
+
+###############################################################################
+# MIT LICENSE
+###############################################################################
+#Copyright 2020 Trip Richert
+#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
