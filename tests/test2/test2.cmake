7134b62936d8f6e4fd0d077020f4934e6da1689c
Trip Richert <trip.richert@gmail.com>
2020-10-03 13:48:57 -0500

    Squashed 'cmake_utils/' changes from 0ab9cad..ee79ffe
    ee79ffe improved documentation and comments
    0568b11 minor changes to documentation describing variable substitution in read_filelist
    6e7b058 added comments
    6253ad0 Revert "Revert "substitution now used by default on filelists""
    7fb3225 I don't think the rollback worked. This renames the correct variable, this time
    b46620a Revert "substitution now used by default on filelists"
    46480a8 changed name of function argument from filelist_varname to headfiles_varname
    4b0e8b5 substitution now used by default on filelists
    0775bd4 changed error check to look at all lines of files before fatally erroring out.
    bdc7b5d updated title of file_functions documentation
    
    git-subtree-dir: cmake_utils
    git-subtree-split: ee79ffe4aba709fb48591663992d15a529b7ef13

--- tests/test2/test2.cmake
+++ tests/test2/test2.cmake
@@ -1,5 +1,5 @@
 get_filename_component(test2_input_file test2/input.txt REALPATH)
-read_filelist(test2_output ${test2_input_file})
+read_filelist_no_substitution(test2_output ${test2_input_file})
 file(WRITE ${CMAKE_BINARY_DIR}/test2.result "${test2_output}\n")
 get_filename_component(test2_expected_file test2/expected.txt.in REALPATH)
 configure_file(${test2_expected_file} test2.expected)
--- tests/test3/test3.cmake
+++ tests/test3/test3.cmake
@@ -1,5 +1,5 @@
 get_filename_component(test3_input_file test3/input.txt REALPATH)
-read_filelist_use_substitution(test3_output ${test3_input_file})
+read_filelist(test3_output ${test3_input_file})
 file(WRITE ${CMAKE_BINARY_DIR}/test3.result "${test3_output}\n")
 get_filename_component(test3_expected_file test3/expected.txt.in REALPATH)
 configure_file(${test3_expected_file} test3.expected)
